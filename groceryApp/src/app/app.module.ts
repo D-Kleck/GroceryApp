1ae0bd21e8b5d97a8604845ea502ac1084413d08
D-Kleck <David@Davids-MacBook-Pro.local>
2019-11-26 10:49:18 -0700

    added components

--- groceryApp/src/app/app.module.ts
+++ groceryApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
